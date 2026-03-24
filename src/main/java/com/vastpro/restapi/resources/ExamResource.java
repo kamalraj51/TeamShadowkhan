0ff07bfb6a8b7f211f27ae1d43b9d7b771536877
RENGANATHAN-2003 <renganmgl2003@gmail.com>
2026-03-24 16:22:04 +0530

    remove

--- src/main/java/com/vastpro/restapi/resources/ExamResource.java
+++ src/main/java/com/vastpro/restapi/resources/ExamResource.java
@@ -1,5 +1,18 @@
 package com.vastpro.restapi.resources;
 
-public class ExamResource {
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 
+@Path("/exam")
+public class ExamResource {
+	
+	@Context
+	private ServletContext servletContext;
+	
+	@Context
+	private HttpServletRequest request;
+	
+	
 }
